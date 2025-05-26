5c136513b672b80ccfadcfa960d0081c74ca1116
Satyam Singh <mesatyamsingh1999@gmail.com>
2025-05-26 18:07:26 +0530

    complete backend user join functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
 import io from "socket.io-client"
 import './App.css'
+import { useState } from "react";
 
 const socket = io("http://localhost:7777");
 
 function App() {
 
+  const [joined,setJoined] = useState(true);
+
+  if(!joined){
+    return (
+      <div>
+        <h1>User is not joined in the Room.</h1>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h1>Welcome to Code Loom.</h1>
+      <h1>User is joined the room successfully.</h1>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ dotenv.config();
 const server = http.createServer(app);
 const port = process.env.PORT || 7777;
 
+//creating room
+const rooms = new Map(); 
 
 
 // socket connection in backend
@@ -20,6 +22,33 @@ const io = new Server(server,{
 
 io.on("connection",(socket)=>{
     console.log("User Connected", socket.id);
+
+    //check if user is currently joind a room or not
+
+    let currentRoom = null;
+    let currentUser = null;
+
+    socket.on("join",({roomId, userName})=>{
+        //if we found user is in currentroom then we make this rule for user
+        if(currentRoom){
+            socket.leave(currentRoom);
+            rooms.get(currentRoom).delete(currentUser);
+            io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+        }
+
+        currentRoom = roomId;
+        currentUser = userName;
+
+        socket.join(roomId);
+
+        if(!rooms.has(roomId)){
+            rooms.set(roomId, new Set());
+        }
+
+        rooms.get(roomId).add(userName)
+
+        io.to(roomId).emit("userJoined", Array.from(rooms.get(currentRoom)));
+    })
 })
 
 
